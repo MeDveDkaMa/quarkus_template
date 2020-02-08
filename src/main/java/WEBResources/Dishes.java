5e260159eef653a848a4f5cc336bf41a1c28a4dc
Alexander Biryulyov <medvedkama@icloud.com>
2020-02-08 17:12:55 +0300

    Rework JDBC Realm for email

--- src/main/java/WEBResources/Dishes.java
+++ src/main/java/WEBResources/Dishes.java
@@ -34,7 +34,7 @@ public class Dishes {
     }
 
     @GET
-    @RolesAllowed({"Admin"})
+    @RolesAllowed({"Admin","User"})
     @Path("dish/GetDish")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Dish> GetDish() {
